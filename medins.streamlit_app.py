84b17e06cd70d1bdbf5f692c510e68a2fb75a6b2
omniaM12 <147537836+omniaM12@users.noreply.github.com>
2023-10-11 17:30:18 +0300

    Update medins.streamlit_app.py

--- medins.streamlit_app.py
+++ medins.streamlit_app.py
@@ -6,13 +6,9 @@ import joblib
  
 poly_reg= joblib.load("poly_reg_model")
 pfull_pipeline=joblib.load("pfull_pipeline")
-
 medins=pd.read_csv("https://github.com/omniaM12/medical_insurance/blob/main/insurance.csv")
-medins=medins.rename(columns={'age':"age", 'sex':'sex',
-                              'bmi':"bmi", "children":"children","region": "region", "smoker":"smoker"})
 st.title("prediction of medical insurance charges")
 st.write("""this is app predics the insurance charges""")
-
 age=st.slider("age", float(medins["age"].min()),float(medins["age"].max()))                     
 bmi=st.slider("bmi", float(medins["bmi"].min()),float(medins["bmi"].max()))
 region= st.selectbox("region",("southeast","southwest","northwest", "northeast"))
@@ -21,7 +17,6 @@ sex=st.selectbox("sex",("male","female"))
 smoker=st.selectbox("smoker",("yes","no"))
 #dict
 user_data={"children":children, "age":age,"bmi":bmi, "region":region,"sex":sex,"smoker":smoker}
-
 inspara=pd.DataFrame(user_data, index=[0])
 inspara_ready=pfull_pipeline.transform(inspara)
 #predict_model
